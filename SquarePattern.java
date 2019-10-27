4d9da84ebf92b72be6d2306b29e1001fa5374904
suchu-rhythm <gyawlisuchitra@gmail.com>
2019-10-27 11:59:14 +0100

    update two files

--- SquarePattern.java
+++ SquarePattern.java
@@ -2,11 +2,9 @@ package com.codewithme;
 
 public class SquarePattern {
     public  static void main(String args[]){
-<<<<<<< HEAD
+
         byte b = 19;
-=======
-        byte b = 15;
->>>>>>> 3066a09efb2187c75ce138e50d165999533c36dc
+
         for (int i = 0; i <= b; i++) {
             for (int j = 0; j <=b; j++) {
                 if(i==0 || i == b || j== 0 || j == b || (i == j)|| (j== (b-i))){
