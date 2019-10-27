1961be1521b2a32544e85dfe4f1c266c48dee626
suchu-rhythm <gyawlisuchitra@gmail.com>
2019-10-27 11:37:26 +0100

    only the number changed

--- SquarePattern.java
+++ SquarePattern.java
@@ -2,7 +2,7 @@ package com.codewithme;
 
 public class SquarePattern {
     public  static void main(String args[]){
-        byte b = 10;
+        byte b = 19;
         for (int i = 0; i <= b; i++) {
             for (int j = 0; j <=b; j++) {
                 if(i==0 || i == b || j== 0 || j == b || (i == j)|| (j== (b-i))){
